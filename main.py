c34900fc1284c9815c8268a0b5fb5aea6793ae8a
Hayyan67 <humairabaig81@gmail.com>
2024-09-22 14:45:28 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+num=random.randint(1,100)
+while True:
+    n=int(input('Guess a number between 1-100: '))
+    if n>num:
+        print('Your number is larger.Try again: ')
+    elif n<num:
+        print('Your number is smaller.Try again: ')
+    elif n==num:
+        print('Your guessed correctly')
+        break
+else:
+    print('Invalid')
+ 
